9eac883cfbdd0b1a8561d686d39fc19c3e7163c8
조시현 <si4018@naver.com>
2024-08-30 10:50:34 +0900

    first commit

---
+++ src/test/java/com/d108/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d108.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
